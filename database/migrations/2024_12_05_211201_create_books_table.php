cb1b62fca238c55beb1bf07d68c78f590105b60b
crgonzalezvi <crgonzalezvi@unal.edu.co>
2024-12-06 00:36:39 -0500

    Carga de primera clase de laravel

---
+++ database/migrations/2024_12_05_211201_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('author_id'); //Crear campo de tipo entero llamado author_id
+            $table->foreign('author_id')->references('id')->on('authors'); //Llave fornanea que apunta al id de authors
+            $table->text('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
